80cfece43bd5b00f820090a8752f518b33a35d21
amm1394 <59041940+AliRezaMazaheri1970@users.noreply.github.com>
2026-02-17 17:08:58 +0330

    تکمیل فاز 1

--- src/Web/BonyadRazavi.WebApp/Components/Shared/DashboardSidebar.razor.cs
+++ src/Web/BonyadRazavi.WebApp/Components/Shared/DashboardSidebar.razor.cs
@@ -4,23 +4,22 @@ namespace BonyadRazavi.WebApp.Components.Shared
 {
     public partial class DashboardSidebar
     {
-
         private sealed record SidebarItem(string Title, string Route);
 
         private static readonly SidebarItem[] SidebarItems =
         [
             new("پذیرش آنلاین + هوش مصنوعی", "/ai-admission"),
-        new("اعلام هزینه", "/cost"),
-        new("پیگیری مرحله آزمایش", "/lab-tracking"),
-        new("دریافت گزارش و صورتحساب", "/Receive-reports-invoices"),
-        new("وضعیت مالی", "/finance"),
-        new("نظرسنجی", "/survey"),
-        new("شکایت", "/complaint"),
-        new("ارسال اطلاعات", "/upload"),
-        new("چت آنلاین", "/chat"),
-        new("دسته بندی مشتریان و امتیازدهی براساس کارکرد", "/customer-ranking"),
-        new("اطلاعات مازاد براساس آخرین گزارش", "/extra-info"),
-        new("ارسال لینک یکبار مصرف توسط پیامک", "/otp-link")
+            new("اعلام هزینه", "/cost"),
+            new("پیگیری مرحله آزمایش", "/lab-tracking"),
+            new("دریافت گزارش و صورتحساب", "/Receive-reports-invoices"),
+            new("وضعیت مالی", "/finance"),
+            new("نظرسنجی", "/survey"),
+            new("شکایت", "/complaint"),
+            new("ارسال اطلاعات", "/upload"),
+            new("چت آنلاین", "/chat"),
+            new("دسته بندی مشتریان و امتیازدهی براساس کارکرد", "/customer-ranking"),
+            new("اطلاعات مازاد براساس آخرین گزارش", "/extra-info"),
+            new("ارسال لینک یکبار مصرف توسط پیامک", "/otp-link")
         ];
 
         private void SignOut()
